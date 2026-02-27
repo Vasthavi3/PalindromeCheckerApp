b3dce88d06065f0884fccea4dd24b41262932215
Vasthavi3 <vd9840@srmist.edu.in>
2026-02-27 14:52:02 +0530

    UC5: Stack based palindrome checker

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
@@ -8,25 +10,24 @@ public class PalindromeCheckerApp {
         System.out.println("Version : 1.0");
         System.out.println();
 
-        // UC4 – Character Array Based Palindrome Check
+        // UC5 – Stack Based Palindrome Check
 
         String word = "madam";
-        char[] characters = word.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        // Push characters into stack
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
+        }
 
         boolean isPalindrome = true;
 
-        int start = 0;
-        int end = characters.length - 1;
-
-        while (start < end) {
-
-            if (characters[start] != characters[end]) {
+        // Pop and compare
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-
-            start++;
-            end--;
         }
 
         if (isPalindrome) {
