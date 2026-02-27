c81c3760f2eb89b12eb7c82d64ca37035e136aa5
Vasthavi3 <vd9840@srmist.edu.in>
2026-02-27 14:01:16 +0530

    Base application setup

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println("      PALINDROME CHECKER APP      ");
+        System.out.println("==================================");
+
+        System.out.println("Version : 1.0");
+        System.out.println("Developed by : Vasthavi");
+        System.out.println();
+
+        System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println("Application Started Successfully!");
+    }
+}
